11530af0d7c8da0977de4b014c785ad90335cd19
nourmohummod91-debug <nourmohummod91@gmail.com>
2026-09-06 14:28:24 +0600

    Create MainActivity.java

---
+++ android/app/src/main/java/com/myra/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.myra.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("MYRA\n\nYour AI Assistant");
+        textView.setTextSize(26);
+        textView.setTextColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER);
+        textView.setBackgroundColor(Color.BLACK);
+
+        setContentView(textView);
+    }
+}
